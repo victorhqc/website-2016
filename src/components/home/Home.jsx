9aaf61c6cb70b832f18cdd4ebc2d58a4663832e8
Victor Castro <victor.castro@glispamedia.com>
2016-12-02 12:03:01 +0100

    Begin basic draft
    Making experiments with styles and fonts. Nothing final
    just seeing how it would actually look

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 export default class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: '100%',
+    };
+
+    this.adjustSize = this.adjustSize.bind(this);
+  }
+
+  componentDidMount() {
+    this.adjustSize();
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.adjustSize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.adjustSize);
+  }
+
+  adjustSize() {
+    const height = window.innerHeight;
+
+    this.setState({ height });
+  }
+
   render() {
+    const { height } = this.state;
+
+    const style = {
+      height,
+    };
+
     return (
-      <div className="Home">
+      <div className="Home" style={style}>
         <div className="Home-logo">
           <h1>VQ</h1>
         </div>
-        <h1>
-          Victor Quiroz Castro <br />
-          <small>
-            JavaScript FullStack Developer
-          </small>
-        </h1>
+        <div className="Home-body">
+          <h1>
+            Victor Quiroz Castro <br />
+            <small>
+              FullStack JavaScript  Developer
+            </small>
+          </h1>
+        </div>
       </div>
     );
   }
